6fe40ea690fa323e469517adc0ac4f776d963ecd
deepskst <deeps.softtest@gmail.com>
2017-08-17 17:08:39 +0530

    TC 003 commiting to the hub

---
+++ uiAutomation/src/test/java/com/tjs/automation/uiAutomation/login/TC_003_DummyTest.java
@@ -0,0 +1,22 @@
+package com.tjs.automation.uiAutomation.login;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+public class TC_003_DummyTest {
+
+	@AfterTest
+	public void setUp() {
+		
+	}
+	
+	@Test
+	public void testPass() {
+		
+	}
+	
+	@AfterTest
+	public void close() {
+		
+	}
+}
